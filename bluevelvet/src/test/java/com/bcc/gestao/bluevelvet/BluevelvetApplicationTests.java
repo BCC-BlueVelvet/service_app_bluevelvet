7391f85ce0cf7e8a21ba6c6ff9824bf2608c025d
Talita Bielecky <talita.bielecky@sou.unifal-mg.edu.br>
2024-11-12 22:58:59 -0300

    Initial commit: Spring project with dependencies and configuration

---
+++ bluevelvet/src/test/java/com/bcc/gestao/bluevelvet/BluevelvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcc.gestao.bluevelvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluevelvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
