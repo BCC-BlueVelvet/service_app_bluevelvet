68154cb0b494bb94c715d94160674ba41f6cf1fb
Vinicius-G0mes <gomes.vinicius@sou.unifal-mg.edu.br>
2024-11-13 19:53:04 -0300

    Injetando o ProductRepository

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/ProductService.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.bcc.gestao.bluevelvet.service;
 
+import com.bcc.gestao.bluevelvet.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
 }
