a92463d54814a3cb1bf026992213eec84a8d3f37
medinastr <pedromstracieri@gmail.com>
2024-11-14 14:00:20 -0300

    adding update method to rest controller

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/ProductRestController.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/ProductRestController.java
@@ -6,6 +6,7 @@ import com.bcc.gestao.bluevelvet.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,12 @@ public class ProductRestController {
 
     @PostMapping("/products")
     public ResponseEntity<Product> save(@RequestBody ProductVO productVO) {
+        Product product = productService.save(productVO);
+        return ResponseEntity.status(201).body(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public  ResponseEntity<Product> update(@RequestBody ProductVO productVO) {
         Product product = productService.save(productVO);
         return ResponseEntity.status(201).body(product);
     }
