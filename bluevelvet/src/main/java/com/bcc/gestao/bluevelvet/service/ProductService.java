9acbb89483b949fdaead6f8ceb6fbe08f00bd508
Talita Bielecky <talita.bielecky@sou.unifal-mg.edu.br>
2024-11-15 15:39:59 -0300

    Adicionando a função save para ProductService

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/ProductService.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.bcc.gestao.bluevelvet.service;
 
+import com.bcc.gestao.bluevelvet.model.entity.Product;
+import com.bcc.gestao.bluevelvet.model.vo.ProductVO;
 import com.bcc.gestao.bluevelvet.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public Product save (ProductVO productvo){
+        Product product = productvo.toEntity();
+        return product;
+    }
 }
