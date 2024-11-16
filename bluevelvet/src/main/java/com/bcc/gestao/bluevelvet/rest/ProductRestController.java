d95b0939055a4de76bbbb2d3d5d166c0f5602bd6
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-16 02:18:07 -0300

    Add endpoint Delete

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/ProductRestController.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/ProductRestController.java
@@ -5,6 +5,8 @@ import com.bcc.gestao.bluevelvet.model.vo.ProductVO;
 import com.bcc.gestao.bluevelvet.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,11 @@ public class ProductRestController {
     public ResponseEntity<Product> save (@RequestBody ProductVO productVO) {
        Product product = productService.save(productVO);
         return ResponseEntity.status(201).body(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        productService.delete(id);  
+        return ResponseEntity.status(204).build();  
     }
 }
