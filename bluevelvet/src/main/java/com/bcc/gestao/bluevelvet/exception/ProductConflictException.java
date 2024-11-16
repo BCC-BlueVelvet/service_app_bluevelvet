42efe0d8319586d4e662b53cbd26901ddf6448a5
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-15 21:24:57 -0300

    feat: add Conflict

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/exception/ProductConflictException.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/exception/ProductConflictException.java
@@ -1,5 +1,16 @@
 package com.bcc.gestao.bluevelvet.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ProductConflictException extends RuntimeException {
-    
+
+    public ProductConflictException() {
+        super("Product conflict occurred");
+    }
+
+    public ProductConflictException(String message) {
+        super(message);
+    }
 }
