40acc5ef225c3513bff43476349e28bf3ee0418b
medinastr <pedromstracieri@gmail.com>
2024-11-18 16:35:40 -0300

    adding tag and operation for findAll

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/swagger/ProductRestControllerSwagger.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/swagger/ProductRestControllerSwagger.java
@@ -2,6 +2,12 @@ package com.bcc.gestao.bluevelvet.rest.swagger;
 
 import com.bcc.gestao.bluevelvet.model.entity.Product;
 import com.bcc.gestao.bluevelvet.model.vo.ProductVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("/api/v1")
+@Tag(name="Product", description = "Endpoints for mapping products of Blue Velvet Music Store.")
 public interface ProductRestControllerSwagger {
 
     @PostMapping("/products")
@@ -29,5 +36,10 @@ public interface ProductRestControllerSwagger {
     public ResponseEntity<Product> update(@PathVariable int id, @RequestBody ProductVO productVO);
 
     @GetMapping("/products")
+    @Operation(responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Product.class)))})
+    })
     public ResponseEntity<List<Product>> findAll();
 }
