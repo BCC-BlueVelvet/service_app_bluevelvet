ec6033c2783e89f5a0a2a9f02e1277d7d21555d3
medinastr <pedromstracieri@gmail.com>
2024-11-25 16:37:55 -0300

    starting user entity

---
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.bcc.gestao.bluevelvet.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private boolean enabled;
+}
