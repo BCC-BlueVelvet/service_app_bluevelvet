7da16a919bdd7ae70bd6c121ad885191b61a0ced
medinastr <pedromstracieri@gmail.com>
2024-11-25 16:33:46 -0300

    adding migration for users and roles

---
+++ bluevelvet/src/main/resources/db/migration/V3__USERS_ROLES.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id INT NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    password varchar(200) NOT NULL,
+    enabled tinyint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE users_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
