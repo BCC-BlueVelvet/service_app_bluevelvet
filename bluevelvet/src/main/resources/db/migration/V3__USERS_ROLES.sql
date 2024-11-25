5412fd2abf175a4c28827ed6edb8d1c2637762ca
medinastr <pedromstracieri@gmail.com>
2024-11-25 17:39:04 -0300

    refactoring roles migration

--- bluevelvet/src/main/resources/db/migration/V3__USERS_ROLES.sql
+++ bluevelvet/src/main/resources/db/migration/V3__USERS_ROLES.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE roles(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) UNIQUE NOT NULL,
     description VARCHAR(200) NOT NULL
 );
@@ -21,3 +21,10 @@ CREATE TABLE users_roles (
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
 );
+
+INSERT INTO roles (name, description) VALUES
+('Administrator', 'Manages everything'),
+('Sales manager', 'Manages product price, customers, shipping, orders and sales report'),
+('Editor', 'Manages categories, brands, products, articles, and menus'),
+('Shipping manager', 'Views products, view orders, and update order status'),
+('Assistant', 'Manages questions and reviews');
