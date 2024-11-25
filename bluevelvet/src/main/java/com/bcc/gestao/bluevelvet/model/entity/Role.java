44b5a8711e0a3e9ca053d238b649f9eefdce7c7d
medinastr <pedromstracieri@gmail.com>
2024-11-25 16:49:58 -0300

    adding many to many to user and role entities

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/model/entity/Role.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/model/entity/Role.java
@@ -1,15 +1,21 @@
 package com.bcc.gestao.bluevelvet.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
@@ -25,4 +31,14 @@ public class Role {
     private String name;
 
     private String description;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH}
+    )
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(name="role_id"),
+            inverseJoinColumns = @JoinColumn(name="user_id")
+    )
+    private List<User> users;
 }
--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/model/entity/User.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/model/entity/User.java
@@ -1,15 +1,21 @@
 package com.bcc.gestao.bluevelvet.model.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -33,4 +39,13 @@ public class User {
     private String password;
 
     private boolean enabled;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
+    private List<Role> roles;
 }
