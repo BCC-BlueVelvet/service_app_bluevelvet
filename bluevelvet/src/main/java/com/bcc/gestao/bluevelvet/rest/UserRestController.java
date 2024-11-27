280ed77e870769275357605893d4cd648bdd572d
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-27 11:34:02 -0300

    feat: endpoint for deleting

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
@@ -1,5 +1,23 @@
 package com.bcc.gestao.bluevelvet.rest;
 
+import com.bcc.gestao.bluevelvet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserRestController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        userService.delete(id);
+        return ResponseEntity.status(204).build();
+    }
 }
