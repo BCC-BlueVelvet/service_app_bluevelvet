5a4762aa8c55d91fe54408de917b116874dbe597
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-27 11:34:52 -0300

    feat: user delete

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/UserService.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/service/UserService.java
@@ -1,5 +1,24 @@
 package com.bcc.gestao.bluevelvet.service;
 
+import com.bcc.gestao.bluevelvet.exception.UserNotFoundException;
+import com.bcc.gestao.bluevelvet.model.entity.User;
+import com.bcc.gestao.bluevelvet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void delete(int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("User not found.");
+        }
+        userRepository.deleteById(id);
+    }
 }
