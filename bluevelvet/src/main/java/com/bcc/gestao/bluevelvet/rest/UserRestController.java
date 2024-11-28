d2864fd56d78959750599f70a6c8c22990973c82
Gustavo M. Costa <gustavomorais1009@gmail.com>
2024-11-28 16:36:31 -0300

    PutMapping

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,12 @@ public class UserRestController {
     public ResponseEntity<UserVO> save(@RequestBody UserVO userVO) {
         UserVO dbUserVO = userService.save(userVO);
         return ResponseEntity.status(HttpStatus.CREATED).body(dbUserVO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserVO> updateUser(@PathVariable int id, @RequestBody UserVO userVO) {
+        UserVO updatedUser = userService.update(id, userVO);
+        return ResponseEntity.ok(updatedUser);
     }
   
     @DeleteMapping("/users/{id}")
