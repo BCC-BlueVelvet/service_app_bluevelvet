00347cf0e01ef793f68c32393053b50ea2e7e572
medinastr <pedromstracieri@gmail.com>
2024-11-28 13:11:04 -0300

    adding save method to user controller

--- bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
+++ bluevelvet/src/main/java/com/bcc/gestao/bluevelvet/rest/UserRestController.java
@@ -4,6 +4,7 @@ import com.bcc.gestao.bluevelvet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.bcc.gestao.bluevelvet.model.vo.UserVO;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,6 +30,12 @@ public class UserRestController {
     public ResponseEntity<UserVO> save(@RequestBody UserVO userVO) {
         UserVO dbUserVO = userService.save(userVO);
         return ResponseEntity.status(HttpStatus.CREATED).body(dbUserVO);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserVO>> findAll() {
+        List<UserVO> dbUserVO = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(dbUserVO);
     }
   
     @DeleteMapping("/users/{id}")
